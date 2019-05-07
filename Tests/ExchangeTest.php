b37548322d4d44c22d6f041e2dc7477eaaddc1c2
Maxime AUBLET <maximeaublet@gmail.com>
2019-05-07 22:11:45 +0200

    finished

--- Tests/ExchangeTest.php
+++ Tests/ExchangeTest.php
@@ -19,21 +19,27 @@ class ExchangeTest extends TestCase
      */
     private $exchange;
 
+
     /**
+     * --------------------------
+     * ----- For the SetUp ------
+     * --------------------------
+     *
+     * Exchange is valid because
+     *  - Dates are valid
+     *  - Receiver valid
+     *  - Product valid
+     *
+     * Email will never be sent because
+     *  - Receiver + 13 yo
+     *
+     * DBConnection sent once
+     *  - Exchange is valid
+     *
      * @throws ReflectionException
      */
     public function setUp(): void
     {
-
-        /**
-         * Exchange valid expected
-         * - Dates are valid
-         * - Receiver valid
-         * - Receiver + 13 yo
-         * - Product valid
-         * - Email never sent
-         * - DBConnection sent once
-         */
         // Mock => Receiver
         $receiver = $this->createMock(User::class);
         $receiver->expects($this->any())->method('isValid')->willReturn(true);
@@ -45,11 +51,11 @@ class ExchangeTest extends TestCase
 
         // Mock Email sender
         $emailSender = $this->createMock(EmailSender::class);
-        $emailSender->expects($this->never())->method('sendEmail')->willReturn(true);
+        $emailSender->expects($this->any())->method('sendEmail')->willReturn(true);
 
         // Mock DBConnection
         $dbConnection = $this->createMock(DBConnection::class);
-        $dbConnection->expects($this->once())->method('saveExchange')->willReturn(true);
+        $dbConnection->expects($this->any())->method('saveExchange')->willReturn(true);
 
         // Dates
         $beginDate = new DateTime("2018-01-01");
@@ -65,92 +71,203 @@ class ExchangeTest extends TestCase
             ->setEmailSender($emailSender);
     }
 
-    /**
-     * @covers Exchange::isDateValid
-     */
-    public function testIsDateValidTrue()
-    {
-        $this->assertTrue($this->exchange->isDateValid());
-    }
 
     /**
-     * @covers Exchange::isValid
-     */
-    public function testIsValidTrue()
-    {
-        $this->assertTrue($this->exchange->isValid());
-    }
-
-    /**
-     * @covers Exchange::isValid
-     * @throws ReflectionException
-     */
-    public function testIsValidFalseBecauseOfReceiver()
-    {
-        $receiverFalse = $this->createMock(User::class);
-        $receiverFalse->expects($this->any())->method('isValid')->willReturn(false);
-        $this->exchange->setProduct($receiverFalse);
-
-        $this->assertFalse($this->exchange->isValid());
-    }
-
-    /**
-     * @covers Exchange::isValid
-     * @throws ReflectionException
-     */
-    public function testIsValidFalseBecauseOfProduct()
-    {
-        $productFalse = $this->createMock(User::class);
-        $productFalse->expects($this->any())->method('isValid')->willReturn(false);
-        $this->exchange->setProduct($productFalse);
-
-        $this->assertFalse($this->exchange->isValid());
-    }
-
-    /**
-     * @covers Exchange::isValid
+     * Exchange saved but no email sent
+     *
+     * @covers Exchange::save
      * @throws Exception
      */
-    public function testSaveTrue()
+    public
+    function testSaveButNotEmail()
     {
         $this->assertTrue($this->exchange->save());
     }
 
-    /**
-     * @throws ReflectionException
-     */
-    public function testExchangeNeverSentOnDB()
-    {
-        $productFalse = $this->createMock(User::class);
-        $productFalse->expects($this->any())->method('isValid')->willReturn(false);
-        $this->exchange->setProduct($productFalse);
 
-        // Mock DBConnection
-        $dbConnectionNeverSent = $this->createMock(DBConnection::class);
-        $dbConnectionNeverSent->expects($this->never())->method('saveExchange');
-        $this->exchange->setDbConnection($dbConnectionNeverSent);
+    /**
+     * Exchange saved not saved, because product not valid, and no email sent
+     *
+     * @covers Exchange::save
+     * @throws ReflectionException
+     * @throws Exception
+     */
+    public
+    function testSaveNotSavedAndNotSent()
+    {
+
+        // No valid product
+        $product = $this->createMock(Product::class);
+        $product->expects($this->any())->method('isValid')->willReturn(false);
+        $this->exchange->setProduct($product);
+
+        // Never called
+        $dbConnection = $this->createMock(DBConnection::class);
+        $dbConnection->expects($this->never())->method('saveExchange')->willReturn(true);
+        $this->exchange->setDbConnection($dbConnection);
+
+        // Never called
+        $emailSender = $this->createMock(EmailSender::class);
+        $emailSender->expects($this->never())->method('sendEmail')->willReturn(true);
+        $this->exchange->setEmailSender($emailSender);
 
         $this->assertFalse($this->exchange->save());
     }
 
+
     /**
+     * Exchange saved and email sent
+     *
+     * @covers Exchange::save
      * @throws ReflectionException
+     * @throws Exception
      */
-    public function testExchangeEmailNeverSent()
+    public
+    function testSaveSavedAndEmail()
     {
-        // Mock => Receiver
+        // Mock => Receiver - 13 yo
         $receiverMinor = $this->createMock(User::class);
         $receiverMinor->expects($this->any())->method("isValid")->willReturn(true);
         $receiverMinor->expects($this->any())->method("getAge")->willReturn(10);
         $this->exchange->setReceiver($receiverMinor);
 
-        // Mock DBConnection
-        $emailNeverSent = $this->createMock(EmailSender::class);
-        $emailNeverSent->expects($this->never())->method('sendEmail');
-        $this->exchange->setDbConnection($emailNeverSent);
+        // Called Once
+        $dbConnection = $this->createMock(DBConnection::class);
+        $dbConnection->expects($this->once())->method('saveExchange')->willReturn(true);
+        $this->exchange->setDbConnection($dbConnection);
 
-        $this->assertFalse($this->exchange->save());
+        // Called One called
+        $emailSender = $this->createMock(EmailSender::class);
+        $emailSender->expects($this->once())->method('sendEmail')->willReturn(true);
+        $this->exchange->setEmailSender($emailSender);
+
+        $this->assertTrue($this->exchange->save());
     }
 
+
+    /**
+     * Dates are valid
+     *
+     * @covers Exchange::isDateValid
+     * @throws ReflectionException
+     */
+    public function testIsDateValidTrue()
+    {
+        // Never called
+        $dbConnection = $this->createMock(DBConnection::class);
+        $dbConnection->expects($this->never())->method('saveExchange')->willReturn(true);
+        $this->exchange->setDbConnection($dbConnection);
+
+        // Never called
+        $emailSender = $this->createMock(EmailSender::class);
+        $emailSender->expects($this->never())->method('sendEmail')->willReturn(true);
+        $this->exchange->setEmailSender($emailSender);
+
+        $this->assertTrue($this->exchange->isDateValid());
+    }
+
+
+    /**
+     * Dates aren't valid
+     *
+     * @covers Exchange::isDateValid
+     * @throws ReflectionException
+     */
+    public function testIsDateValidFalse()
+    {
+        // Never called
+        $dbConnection = $this->createMock(DBConnection::class);
+        $dbConnection->expects($this->never())->method('saveExchange')->willReturn(true);
+        $this->exchange->setDbConnection($dbConnection);
+
+        // Never called
+        $emailSender = $this->createMock(EmailSender::class);
+        $emailSender->expects($this->never())->method('sendEmail')->willReturn(true);
+        $this->exchange->setEmailSender($emailSender);
+
+        $this->exchange->setBeginDate(new DateTime("2018-01-01"));
+        $this->exchange->setEndDate(new DateTime("2017-01-01"));
+
+        $this->assertFalse($this->exchange->isDateValid());
+    }
+
+
+    /**
+     * Exchange is valid
+     *
+     * @covers Exchange::isValid
+     * @throws ReflectionException
+     */
+    public
+    function testIsValidTrue()
+    {
+        // Never called
+        $dbConnection = $this->createMock(DBConnection::class);
+        $dbConnection->expects($this->never())->method('saveExchange')->willReturn(true);
+        $this->exchange->setDbConnection($dbConnection);
+
+        // Never called
+        $emailSender = $this->createMock(EmailSender::class);
+        $emailSender->expects($this->never())->method('sendEmail')->willReturn(true);
+        $this->exchange->setEmailSender($emailSender);
+
+        $this->assertTrue($this->exchange->isValid());
+    }
+
+
+    /**
+     * Exchange not valid because receiver is not
+     *
+     * @covers Exchange::isValid
+     * @throws ReflectionException
+     */
+    public
+    function testIsValidFalseBecauseOfReceiver()
+    {
+        // False receiver
+        $receiver = $this->createMock(User::class);
+        $receiver->expects($this->any())->method('isValid')->willReturn(false);
+        $this->exchange->setProduct($receiver);
+
+        // Never called
+        $dbConnection = $this->createMock(DBConnection::class);
+        $dbConnection->expects($this->never())->method('saveExchange')->willReturn(true);
+        $this->exchange->setDbConnection($dbConnection);
+
+        // Never called
+        $emailSender = $this->createMock(EmailSender::class);
+        $emailSender->expects($this->never())->method('sendEmail')->willReturn(true);
+        $this->exchange->setEmailSender($emailSender);
+
+        $this->assertFalse($this->exchange->isValid());
+    }
+
+
+    /**
+     * Exchange not valid because product is not
+     *
+     * @covers Exchange::isValid
+     * @throws ReflectionException
+     */
+    public
+    function testIsValidFalseBecauseOfProduct()
+    {
+        // False Product
+        $product = $this->createMock(Product::class);
+        $product->expects($this->any())->method('isValid')->willReturn(false);
+        $this->exchange->setProduct($product);
+
+        // Never called
+        $dbConnection = $this->createMock(DBConnection::class);
+        $dbConnection->expects($this->never())->method('saveExchange')->willReturn(true);
+        $this->exchange->setDbConnection($dbConnection);
+
+        // Never called
+        $emailSender = $this->createMock(EmailSender::class);
+        $emailSender->expects($this->never())->method('sendEmail')->willReturn(true);
+        $this->exchange->setEmailSender($emailSender);
+
+        $this->assertFalse($this->exchange->isValid());
+    }
 
 }
